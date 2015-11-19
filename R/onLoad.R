9ec6916362ed22e7e8a8620ee1216dc39eabb03d
soeque1 <soeque1@gmail.com>
2015-11-19 18:22:40 +0900

    트위터 경로 버그 수정

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,27 +1,33 @@
 .KoNLPQEnv <- new.env()
 
-.onLoad <-
-function(lib, pkg  = "KoNLPQ") {
-    .jinit()
-    tempDir <- find.package(pkg)
+.onLoad <- function(libname, pkgname) {
+    tempDir <- find.package(pkgname)
     
-    pathToSdk <- paste0(system.file(package = pkg) , tempDir, '/java/')
+    pathToSdk <- paste0(system.file(package = pkgname) , tempDir, '/inst/java/')
     
-    jarPaths <- c(paste0(pathToSdk, "kkma-2.0.jar"),
-                  paste0(pathToSdk, "lucypark.jar"),
-                  paste0(pathToSdk, "korean-text-3.0.jar"),
-                  paste0(pathToSdk, "scala-library-2.11.6.jar"),
-                  paste0(pathToSdk, "twitter-text-1.11.1.jar"))
+    #jarPaths <- c(paste0(pathToSdk, "kkma-2.0.jar"),
+    #              paste0(pathToSdk, "lucypark.jar"),
+    #              paste0(pathToSdk, "korean-text-3.0.jar"),
+    #              paste0(pathToSdk, "scala-library-2.11.6.jar"),
+    #              paste0(pathToSdk, "twitter-text-1.11.1.jar"))
     
-    .jpackage(pkg, morePaths=jarPaths)
+    .jpackage(pkgname, morePaths=pathToSdk)
     #attach( javaImport( c("java.lang", "java.io", "java.mail", "java.util", "java.text")))
     
     packageStartupMessage( paste( "KoNLPQ loaded. The classpath is: ", paste(.jclassPath(), collapse=" " ) ) )        
 }
 
 .onAttach <- function(libname, pkgname){
+
   if(!exists("KkmaObj", envir=KoNLPQ:::.KoNLPQEnv)){
     assign("KkmaObj",.jnew("kr.lucypark.kkma.KkmaInterface"), KoNLPQ:::.KoNLPQEnv)
   }
+  
+  if(!exists("twitterObj", envir=KoNLPQ:::.KoNLPQEnv)){
+	twitterObj <- .jnew('com.twitter.penguin.korean.TwitterKoreanProcessorJava$Builder')
+	twitterObj <- .jrcall(twitterObj, 'build')
+    assign("twitterObj",twitterObj, KoNLPQ:::.KoNLPQEnv)
+  }  
+  
 }
 
