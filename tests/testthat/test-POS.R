f4b0783123226c9d635f509e953ff46f371f44f1
soeque1 <soeque1@gmail.com>
2015-12-29 14:21:07 +0900

    no message

--- tests/testthat/test-POS.R
+++ tests/testthat/test-POS.R
@@ -5,8 +5,8 @@ strings <- '오늘도 술로 밤을 채우고'
 noun.kkma <- kkma.extractPOS(strings, "N")
 expect_equal(noun.kkma, c('오늘', '술', '밤'))
 
-twi.kkma <- twi.extractPOS(strings, "N")
-expect_equal(noun.kkma, c('오늘', '술', '밤'))
+noun.twi <- twi.extractPOS(strings, "N")
+expect_equal(noun.twi, c('오늘', '술', '밤'))
 })
 
 test_that("extractVerb", {
@@ -14,8 +14,8 @@ strings <- '오늘도 술로 밤을 채우고'
 verb.kkma <- kkma.extractPOS(strings, "V")
 expect_equal(verb.kkma, c('오', '채우'))
 
-twi.kkma <- twi.extractPOS(strings, "V")
-expect_equal(verb.kkma, c('채우다'))
+verb.twi <- twi.extractPOS(strings, "V")
+expect_equal(verb.twi, c('채우다'))
 })
 
 
